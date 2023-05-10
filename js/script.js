1fd4ca626fc0fd36632aee14f9ec1550e25ded12
CheeMinHao <cheemh3456@gmail.com>
2023-05-10 22:56:11 +1000

    more changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 HEADERS_LENGTH = 4;
 NAVBAR_LENGTH = 5;
+FOOTER_LENGTH = 17;
 BODY_LENGTH = 12;
 
 var headerList = [];
@@ -17,6 +18,11 @@ for (i=0 ; i < BODY_LENGTH; i++) {
     bodyList.push(document.getElementById("body-font" + String(i + 1)));
 }
 
+var footerList = [];
+for (i=0 ; i < FOOTER_LENGTH; i++) {
+    footerList.push(document.getElementById("footer-font" + String(i + 1)));
+}
+
 function changeToSmall() {
     for (i=0 ; i < HEADERS_LENGTH; i++) {
         headerList[i].style.fontSize = "x-small";
@@ -26,6 +32,9 @@ function changeToSmall() {
     }
     for (i=0 ; i < BODY_LENGTH; i++) {
         bodyList[i].style.fontSize = "x-small";
+    }
+    for (i=0 ; i < FOOTER_LENGTH; i++) {
+        footerList[i].style.fontSize = "x-small";
     }
 }
 
@@ -39,6 +48,9 @@ function changeToDefault() {
     for (i=0 ; i < BODY_LENGTH; i++) {
         bodyList[i].style.fontSize = "medium";
     }
+    for (i=0 ; i < FOOTER_LENGTH; i++) {
+        footerList[i].style.fontSize = "medium";
+    }
 }
 
 function changeToLarge() {
@@ -50,5 +62,8 @@ function changeToLarge() {
     }
     for (i=0 ; i < BODY_LENGTH; i++) {
         bodyList[i].style.fontSize = "x-large";
+    }
+    for (i=0 ; i < FOOTER_LENGTH; i++) {
+        footerList[i].style.fontSize = "x-large";
     }
 }
