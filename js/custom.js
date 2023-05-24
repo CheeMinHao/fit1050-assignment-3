7f7c21a2b8469024562244ab8deda3a81ce1db7b
CheeMinHao <cheemh3456@gmail.com>
2023-05-24 18:18:42 +1000

    major changes

--- js/custom.js
+++ js/custom.js
@@ -10,9 +10,9 @@
 
     // BACKSTRETCH SLIDESHOW
     $('.hero-section').backstretch([
-      "images/slideshow/afro-woman-cleaning-window-with-rag-home.jpg", 
-      "images/slideshow/afro-woman-holding-bucket-with-cleaning-items.jpg",
-      "images/slideshow/unrecognizable-cleaner-walking-into-hotel-room-with-tools-detergents.jpg"
+      "images/slideshow/mop-floor.jpg", 
+      "images/slideshow/cloth-wipe-table.jpg",
+      "images/slideshow/vacuum-floor.jpg"
     ],  {duration: 2000, fade: 750});
     
     // CUSTOM LINK
